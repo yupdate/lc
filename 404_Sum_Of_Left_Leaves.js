113dfe869a1e1ebdd993b3ba67290bc4f6b13b4a
yupdate <yizhouzh233@gmail.com>
2017-02-28 23:38:39 -0800

    Create 404_Sum_Of_Left_Leaves.js

---
+++ 404_Sum_Of_Left_Leaves.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumOfLeftLeaves = function(root) {
+    if(!root) {
+        return 0;
+    }
+    
+    return (root.left && !root.left.left && !root.left.right ? root.left.val : 0) + sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+};
